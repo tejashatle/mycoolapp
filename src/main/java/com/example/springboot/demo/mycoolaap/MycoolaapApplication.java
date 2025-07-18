6a0c7a3c7ceb67360d1a1ce731f1999c82c3bbe3
tejashatle <tejashatle2@gmail.com>
2025-07-18 18:50:56 +0530

    Rest Inital Commit

---
+++ src/main/java/com/example/springboot/demo/mycoolaap/MycoolaapApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.mycoolaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolaapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolaapApplication.class, args);
+	}
+
+}
