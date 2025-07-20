c625577d7eb378fc9bac3abafbd8cf127ca162fc
tejashatle <tejashatle3@gmail.com>
2025-07-20 17:23:16 +0530

    Spring devtools

--- src/main/java/com/example/springboot/demo/mycoolaap/rest/FunRestController.java
+++ src/main/java/com/example/springboot/demo/mycoolaap/rest/FunRestController.java
@@ -10,4 +10,14 @@ public class FunRestController {
 	public String sayHello() {
 		return "Hello World!";
 	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "I do workout for 2 hrs.";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
+	}
 }
