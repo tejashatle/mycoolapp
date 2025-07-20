93be3b26c9683417d8e00a9b53f06e05f2030980
tejashatle <tejashatle3@gmail.com>
2025-07-20 19:42:36 +0530

    Spring boot

--- src/main/java/com/example/springboot/demo/mycoolaap/rest/FunRestController.java
+++ src/main/java/com/example/springboot/demo/mycoolaap/rest/FunRestController.java
@@ -1,14 +1,27 @@
 package com.example.springboot.demo.mycoolaap.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	@Value("${coach.name}")
+	private String coachName;
 
+	@Value("${team.name}")
+	private String teamName;
+	
+	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World!";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String teamInfo() {
+		return "Coach name: "+ coachName +" Team Name: " + teamName;
 	}
 	
 	@GetMapping("/workout")
